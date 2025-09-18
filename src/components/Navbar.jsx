5202516e820f94f491cf78a143d246839d15151e
Luis Guilherme da Cruz <guicruz95@hotmail.com>
2025-09-18 15:50:16 -0300

    Finalizei o primeiro designer do NAVBAR

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,15 @@
 import { CgMenuGridO } from "react-icons/cg";
-import styles from '../styles/Navbar.module.css';
-
+import styles from "../styles/Navbar.module.css";
 
 function Navbar() {
   return (
     <div className={styles.navbar_container}>
       <h1>LG</h1>
-      <h2>LUISGUILHERME</h2>
-      <CgMenuGridO className={styles.menu_icon}/>
+      <div className={styles.text_title}>
+        <h2>LUISGUILHERME</h2>
+        <h3>DESENVOLVEDOR BACK-END</h3>
+      </div>
+      <CgMenuGridO className={styles.menu_icon} />
     </div>
   );
 }
